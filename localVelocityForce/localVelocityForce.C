7d390e3e47594a5d7bf4695469f2df5313a4ce8d
gaberchen <gaberchen@web.de>
2017-03-21 12:25:53 +0100

    Add files via upload

---
+++ localVelocityForce/localVelocityForce.C
@@ -0,0 +1,296 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "localVelocityForce.H"
+#include "fvMatrices.H"
+#include "DimensionedField.H"
+#include "IFstream.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * Static Member Functions * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace fv
+{
+    defineTypeNameAndDebug(localVelocityForce, 0);
+
+    addToRunTimeSelectionTable
+    (
+        option,
+        localVelocityForce,
+        dictionary
+    );
+}
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::fv::localVelocityForce::writePropslocal
+(
+    scalarField gradPlocal_
+) const
+{
+    // Only write on output time
+    if (mesh_.time().outputTime())
+    {
+        IOdictionary propsDict
+        (
+            IOobject
+            (
+                name_ + "_gradPlocal",
+                mesh_.time().timeName(),
+                mesh_,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            )
+        );
+        propsDict.add("gradient", gradPlocal_);
+        propsDict.regIOobject::write();
+     }
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::fv::localVelocityForce::localVelocityForce
+(
+    const word& sourceName,
+    const word& modelType,
+    const dictionary& dict,
+    const fvMesh& mesh
+)
+:
+    cellSetOption(sourceName, modelType, dict, mesh),
+    Ubar_(coeffs_.lookup("Ubar")),
+    gradP0_(0.0),
+    dGradP_(0.0),
+    flowDir_(Ubar_/mag(Ubar_)),
+    relaxation_(coeffs_.lookupOrDefault<scalar>("relaxation", 1.0)),
+    rAPtr_(NULL)
+
+{
+    coeffs_.lookup("fields") >> fieldNames_;
+    coeffs_.lookup("freestream") >> freestreamVel_;
+    coeffs_.lookup("position") >> position_;
+    coeffs_.lookup("gaussFactor") >> gaussFactor_;
+    coeffs_.lookup("gaussType") >> gaussType_;
+    coeffs_.lookup("diameter") >> diameter_;
+    coeffs_.lookup("height") >> height_;
+
+    if (fieldNames_.size() != 1)
+    {
+        FatalErrorIn
+        (
+            "Foam::fv::localVelocityForce::"
+            "localVelocityForce"
+            "("
+                "const word&, "
+                "const word&, "
+                "const dictionary&, "
+                "const fvMesh&"
+            ")"
+        )   << "Source can only be applied to a single field.  Current "
+            << "settings are:" << fieldNames_ << exit(FatalError);
+    }
+
+    applied_.setSize(fieldNames_.size(), false);
+
+    // Read the initial pressure gradient from file if it exists
+    IFstream propsFile
+    (
+        mesh_.time().timePath()/"uniform"/(name_ + "Properties")
+    );
+
+    if (propsFile.good())
+    {
+        Info<< "    Reading pressure gradient from file" << endl;
+        dictionary propsDict(dictionary::null, propsFile);
+        propsDict.lookup("gradient") >> gradP0_;
+    }
+
+    Info<< "    Initial pressure gradient = " << gradP0_ << nl << endl;
+
+
+        // Read nu from object registry
+    const dictionary& transportProperties = mesh_.lookupObject<IOdictionary>
+    (
+        "transportProperties"
+    );
+    dimensionedScalar nu;
+    transportProperties.lookup("nu") >> nu;
+    nu_ = nu.value();
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+
+Foam::scalarField Foam::fv::localVelocityForce::magUbarlocal
+(
+    const volVectorField& U
+) const
+{
+    scalarField magUbarlocal_(cells_.size(), 0.0);
+
+    const scalarField& cv = mesh_.V();
+    forAll(cells_, i)
+    {
+        label cellI = cells_[i];
+        magUbarlocal_[i] = (flowDir_ & U[cellI]);
+    }
+    
+    //reduce(magUbarlocal_,sumOp<scalarField>());
+
+    return magUbarlocal_;
+}
+
+
+void Foam::fv::localVelocityForce::correct(volVectorField& U)
+{
+    const scalarField& rAU = rAPtr_().internalField();//1.0/UEqn.A() Diagonal part of matrix U [s/m]
+
+	// Integrate flow variables over cell set
+	scalarField rAUlocal_(cells_.size(), 0.0);
+
+	forAll(cells_, i)
+	{
+		label cellI = cells_[i];
+		rAUlocal_[i] = rAU[cellI];
+	}
+	
+	//reduce(rAUlocal_,sumOp<scalarField>());
+
+	// Calculate the pressure gradient increment needed to adjust the average
+	// flow-rate to the desired value
+
+	scalarField magUBarLocal = magUbarlocal(U);
+
+	dGradPlocal_ = relaxation_*(mag(Ubar_) - magUBarLocal)/(rAUlocal_);
+
+	// Apply correction to velocity field
+	forAll(cells_, i)
+	{
+		label cellI = cells_[i];
+		U[cellI] =  flowDir_ * (rAU[cellI] * dGradPlocal_[cellI]);
+	}
+
+	scalarField gradPlocal_ = gradP0_+ dGradPlocal_;
+	
+
+	writePropslocal(gradPlocal_);
+
+	if (debug == 1)
+	{
+		Pout << "cells_ :" << cells_ << endl;
+		Pout << "rAUlocal :" << rAUlocal_ << endl;
+		Pout << "magUBarLocal :" << magUBarLocal << endl;
+		Pout << "dGradPlocal_ :" << dGradPlocal_ << endl;
+		Pout << "gradPlocal_ :" << gradPlocal_ << endl;
+	}
+	
+	Info << "Adding source for : " << cellSetName_ << endl;
+}
+
+
+void Foam::fv::localVelocityForce::addSup
+(
+	scalarField dgradPlocal_,
+    fvMatrix<vector>& eqn,
+    const label fieldI
+)
+{
+    DimensionedField<vector, volMesh> Su
+    (
+        IOobject
+        (
+            name_ + fieldNames_[fieldI] + "_Su",
+            mesh_.time().timeName(),
+            mesh_,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh_,
+        dimensionedVector("zero", eqn.dimensions()/dimVolume, vector::zero)
+    );
+
+	scalarField gradPlocal_ = gradP0_+ dGradPlocal_;
+	
+	UIndirectList<vector>(Su, cells_) = flowDir_*gradPlocal_;
+
+
+    eqn += Su;
+}
+
+
+void Foam::fv::localVelocityForce::addSup
+(
+	scalarField dgradPlocal_,
+    const volScalarField& rho,
+    fvMatrix<vector>& eqn,
+    const label fieldI
+)
+{
+
+    this->addSup(dgradPlocal_, eqn, fieldI);
+}
+
+
+void Foam::fv::localVelocityForce::constrain
+(
+    fvMatrix<vector>& eqn,
+    const label
+)
+{
+    if (rAPtr_.empty())
+    {
+        rAPtr_.reset
+        (
+            new volScalarField
+            (
+                IOobject
+                (
+                    name_ + ":rA",
+                    mesh_.time().timeName(),
+                    mesh_,
+                    IOobject::NO_READ,
+                    IOobject::NO_WRITE
+                ),
+                1.0/eqn.A()
+            )
+        );
+    }
+    else
+    {
+        rAPtr_() = 1.0/eqn.A();
+    }
+
+    gradP0_ = dGradPlocal_;
+    dGradPlocal_ = 0.0;
+}
+
+
+// ************************************************************************* //
